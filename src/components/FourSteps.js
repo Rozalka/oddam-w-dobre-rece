bb708e97b37356d48f770052ab429ab2f00e9d5a
Rozalia Przeworska <52925130+Rozalka@users.noreply.github.com>
2020-11-03 20:43:32 +0100

    add foursteps.scss

--- src/components/FourSteps.js
+++ src/components/FourSteps.js
@@ -3,26 +3,35 @@ import React from 'react'
 function FourSteps() {
     return (
         <section className="steps">
-            <h2>Wystarczą 4 proste kroki</h2>
+            <h2 className="steps-header">Wystarczą 4 proste kroki</h2>
+            <div className="decor"></div>
             <div className="simple-steps">
-                <div>
-                    <div></div>
-                    <h4>Wybierz rzeczy</h4>
+                <div className="single-box">
+                    <div className="icon icon1"></div>
+                    <div className="single-box-header">
+                        <h4>Wybierz rzeczy</h4>
+                    </div>
                     <p>ubrania, zabawki, sprzęt i inne</p>
                 </div>
-                <div>
-                    <div></div>
-                    <h4>Spakuj je</h4>
+                <div className="single-box">
+                    <div className="icon icon2"></div>
+                    <div className="single-box-header">
+                        <h4>Spakuj je</h4>
+                    </div>
                     <p>skorzystaj z worków na śmieci</p>
                 </div>
-                <div>
-                    <div></div>
-                    <h4>Zdecyduj komu chcesz pomóc</h4>
+                <div className="single-box">
+                    <div className="icon icon3"></div>
+                    <div className="single-box-header">
+                        <h4>Zdecyduj komu chcesz pomóc</h4>
+                    </div>
                     <p>wybierz zaufane miejsce</p>
                 </div>
-                <div>
-                    <div></div>
-                    <h4>Zamów kuriera</h4>
+                <div className="single-box">
+                    <div className="icon icon4"></div>
+                    <div className="single-box-header">
+                        <h4>Zamów kuriera</h4>
+                    </div>
                     <p>kurier przyjedzie w dogodnym terminie</p>
                 </div>
 
