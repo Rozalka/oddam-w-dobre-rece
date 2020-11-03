6ec90bd5f74afe5f4f79430ffe62a26affd7f66c
Rozalia Przeworska <52925130+Rozalka@users.noreply.github.com>
2020-11-03 18:45:09 +0100

    add FourSteps.js

--- src/components/FourSteps.js
+++ src/components/FourSteps.js
@@ -2,9 +2,32 @@ import React from 'react'
 
 function FourSteps() {
     return (
-        <div>
-            
-        </div>
+        <section className="steps">
+            <h2>Wystarczą 4 proste kroki</h2>
+            <div className="simple-steps">
+                <div>
+                    <div></div>
+                    <h4>Wybierz rzeczy</h4>
+                    <p>ubrania, zabawki, sprzęt i inne</p>
+                </div>
+                <div>
+                    <div></div>
+                    <h4>Spakuj je</h4>
+                    <p>skorzystaj z worków na śmieci</p>
+                </div>
+                <div>
+                    <div></div>
+                    <h4>Zdecyduj komu chcesz pomóc</h4>
+                    <p>wybierz zaufane miejsce</p>
+                </div>
+                <div>
+                    <div></div>
+                    <h4>Zamów kuriera</h4>
+                    <p>kurier przyjedzie w dogodnym terminie</p>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
