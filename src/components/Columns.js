9c9df8d4a82105d59ceab8a446763a841e81bdf8
Rozalia Przeworska <52925130+Rozalka@users.noreply.github.com>
2020-11-03 18:08:34 +0100

    add columns.js

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 function Columns() {
     return (
-        <div className="three-columns">
-            <div>
+        <section className="three-columns">
+            <div className="single-column">
                 <h2 className="statistics">10</h2>
                 <h3 className="statistic-header">oddanych worków</h3>
                 <p className="statistic-details">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div>
+            <div className="single-column">
                 <h2 className="statistics">5</h2>
                 <h3 className="statistic-header">wspartych organizacji</h3>
                 <p className="statistic-details">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div>
+            <div className="single-column">
                 <h2 className="statistics">7</h2>
                 <h3 className="statistic-header">zorganizowanych zbiórek</h3>
                 <p className="statistic-details">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-        </div>
+        </section>
     )
 }
 
