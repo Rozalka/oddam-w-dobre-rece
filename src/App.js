f1157dc81fcd50cf74f6fa873392c7519abff29e
Rozalia Przeworska <52925130+Rozalka@users.noreply.github.com>
2020-11-22 18:00:33 +0100

    add message send

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Home from './components/Home/Home';
-import LogIn from './components/LogIn/LogIn';
-import CreateAccount from './components/CreateAccount/CreateAccount';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LogOut from './components/LogOut/LogOut';
+import Home from "./components/Home/Home";
+import LogIn from "./components/LogIn/LogIn";
+import CreateAccount from "./components/CreateAccount/CreateAccount";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LogOut from "./components/LogOut/LogOut";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/logIn">
-          <LogIn/>
-        </Route>
-        <Route path="/createAccount">
-          <CreateAccount/>
-        </Route>
-        <Route path="/logOut">
-          <LogOut/>
-        </Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/logIn">
+            <LogIn />
+          </Route>
+          <Route path="/createAccount">
+            <CreateAccount />
+          </Route>
+          <Route path="/logOut">
+            <LogOut />
+          </Route>
         </Switch>
       </Router>
     </div>
